9fa33ea7a580bed4f0449286173ffb073eb484cf
Tushar Goyal <tgoyal2001@gmail.com>
2025-04-30 04:22:35 +0530

    changed the app icon, changed the startup screen icon and theme. Created a new splash screen and linked with the main screen.

--- app/src/main/java/com/example/leafdiseaseapp/SplashScreen.kt
+++ app/src/main/java/com/example/leafdiseaseapp/SplashScreen.kt
@@ -1,20 +1,20 @@
 package com.example.leafdiseaseapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }, 5000) // 5 seconds
     }
 }
